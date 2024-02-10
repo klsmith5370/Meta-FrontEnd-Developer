ffe7f7adafa929cb03c419c6e334d8636b43a207
Kourtney Smith <klsmith5370@gmail.com>
2024-02-10 18:25:06 -0500

    Completed function program for course

--- JavaScript/FunctionalProgram.js
+++ JavaScript/FunctionalProgram.js
@@ -1,11 +1,10 @@
 // Task 1: Build a function-based console log message generator
 function consoleStyler(color, background, fontSize, txt) {
-    var message = '%c' + txt;
+    var message = "%c" + txt;
     var style = `color: ${color};`;
     style += `background: ${background};`;
     style += `font-size: ${fontSize};`;
     console.log(message, style);
-
 }
 
 // Task 2: Build another console log message generator
@@ -13,7 +12,7 @@ function celebrateStyler(reason) {
     var fontStyle = "color: tomato; font-size: 50px";
     if (reason == "birthday") {
         console.log(`%cHappy Birthday`, fontStyle);
-    } else if (reason == 'champions') {
+    } else if (reason == "champions") {
         console.log(`%cCongrats on the title!`, fontStyle);
     } else {
         console.log(`%c${reason}`, fontStyle);
@@ -22,11 +21,13 @@ function celebrateStyler(reason) {
 
 // Task 3: Run both the consoleStyler and the celebrateStyler functions
 consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+
 celebrateStyler("birthday");
 
 // Task 4: Insert a congratulatory and custom message
 function styleAndCelebrate(color, background, fontSize, txt, reason) {
     consoleStyler(color, background, fontSize, txt);
+
     celebrateStyler(reason);
 }
 // Call styleAndCelebrate
