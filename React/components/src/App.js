343b50e57b4e18afcaac2ce9e321a3ca059cc5da
Kourtney Smith <klsmith5370@gmail.com>
2024-07-25 16:03:32 -0400

    Practiced stylingReact components

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,13 +1,27 @@
-import Heading from './Heading'
+import "./App.css";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className='App'>
-      <Heading firstName='Kara'/>
-      <Heading firstName='Bob'/>
+    <div>
+      <h1>"Task: Add 3 cards"</h1>
+      <Card h2='First card h2' h3='First card h3'/>
+      <Card h2='Second card h2' h3='Second card h3'/>
+      <Card h2='Third card h2' h3='Third card h3'/>
     </div>
   );
-
 }
 
 export default App;
+
+// import Heading from './Heading'
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Heading firstName='Kara'/>
+//       <Heading firstName='Bob'/>
+//     </div>
+//   );
+
+// }
