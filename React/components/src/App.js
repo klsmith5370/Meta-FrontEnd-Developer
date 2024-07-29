65acdf25a081aa9120706a3a258e032254087d78
Kourtney Smith <klsmith5370@gmail.com>
2024-07-29 16:47:24 -0400

    Setting up practice with event handlers

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
-import Card from "./Card";
 
 function App() {
   return (
     <div>
-      <h1>"Task: Add 3 cards"</h1>
-      <Card h2='First card h2' h3='First card h3'/>
-      <Card h2='Second card h2' h3='Second card h3'/>
-      <Card h2='Third card h2' h3='Third card h3'/>
+      <h1>Task: Add a button and handle a click event</h1>
     </div>
   );
 }
 
 export default App;
 
+// * Using props to create multiple instances
+// import Card from "./Card";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>"Task: Add 3 cards"</h1>
+//       <Card h2='First card h2' h3='First card h3'/>
+//       <Card h2='Second card h2' h3='Second card h3'/>
+//       <Card h2='Third card h2' h3='Third card h3'/>
+//     </div>
+//   );
+// }
+
+
+// * Using props
 // import Heading from './Heading'
 
 // function App() {
