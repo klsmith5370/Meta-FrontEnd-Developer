68613e09c49baa162cd927529a2829147457f705
Kourtney Smith <klsmith5370@gmail.com>
2024-07-29 16:51:16 -0400

    Practiced event handlers using the onClick function

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,9 +1,19 @@
-import "./App.css";
 
+
+// * Practice using event handlers
 function App() {
+  function handleClick() {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt('type a number'); 
+    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+  }
+
   return (
     <div>
       <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3.</button>
+
     </div>
   );
 }
@@ -11,6 +21,7 @@ function App() {
 export default App;
 
 // * Using props to create multiple instances
+// import "./App.css";
 // import Card from "./Card";
 
 // function App() {
