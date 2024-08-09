0fb0853a7271f845213da2ebda61660dea59fd12
Kourtney Smith <klsmith5370@gmail.com>
2024-08-09 18:44:24 -0400

    Commented out previous code, practicing with using state

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,24 +1,38 @@
+// *Practice using state
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
 
-
-// * Practice using event handlers
 function App() {
-  function handleClick() {
-    let randomNum = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNum);
-    let userInput = prompt('type a number'); 
-    alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
-  }
-
   return (
-    <div>
-      <h1>Task: Add a button and handle a click event</h1>
-      <button onClick={handleClick}>Guess the number between 1 and 3.</button>
-
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits />
+      <FruitsCounter />
     </div>
   );
-}
+};
 
 export default App;
+
+// * Practice using event handlers
+// function App() {
+//   function handleClick() {
+//     let randomNum = Math.floor(Math.random() * 3) + 1;
+//     console.log(randomNum);
+//     let userInput = prompt('type a number'); 
+//     alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+//   }
+
+//   return (
+//     <div>
+//       <h1>Task: Add a button and handle a click event</h1>
+//       <button onClick={handleClick}>Guess the number between 1 and 3.</button>
+
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // * Using props to create multiple instances
 // import "./App.css";
