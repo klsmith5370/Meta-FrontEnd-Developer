90a0a44a2b8ef93a3f7c626a6ffbafddbefb5cce
Kourtney Smith <klsmith5370@gmail.com>
2024-08-12 15:32:52 -0400

    Added completed folder for completed practice, practiced using routes with React

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,25 +1,52 @@
-// *Practice using state
-import React from "react";
-import Fruits from "./Fruits";
-import FruitsCounter from "./FruitsCounter";
+// * Practice with using React Router
+import "./App.css";
+import Homepage from "./Homepage";
+import AboutLittleLemon from "./AboutLittleLemon";
+import { Routes, Route, Link } from "react-router-dom";
+import Contact from "./Contact";
 
 function App() {
-  const [fruits] = React.useState([
-      {fruitName: 'apple', id: 1},
-      {fruitName: 'apple', id: 2},
-      {fruitName: 'plum', id: 3},
-  ]);
-
   return (
-    <div className="App">
-      <h1>Where should the state go?</h1>
-      <Fruits fruits={fruits} />
-      <FruitsCounter fruits={fruits} />
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Homepage</Link>
+      <Link to="/about" className="nav-item">About Little Lemon</Link>
+      <Link to="/contact" className="nav-item">Contact</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/about" element={<AboutLittleLemon />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
+
+// *Practice using state
+
+// import React from "react";
+// import Fruits from "./Fruits";
+// import FruitsCounter from "./FruitsCounter";
+
+// function App() {
+//   const [fruits] = React.useState([
+//       {fruitName: 'apple', id: 1},
+//       {fruitName: 'apple', id: 2},
+//       {fruitName: 'plum', id: 3},
+//   ]);
+
+//   return (
+//     <div className="App">
+//       <h1>Where should the state go?</h1>
+//       <Fruits fruits={fruits} />
+//       <FruitsCounter fruits={fruits} />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // * Practice using event handlers
 // function App() {
--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import {BrowserRouter} from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
-    <App />  
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render( 
+//     <App />  
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
