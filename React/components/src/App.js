0b99645f502c455bc019c3a017cc58ed6bb5cf67
Kourtney Smith <klsmith5370@gmail.com>
2024-08-12 15:56:21 -0400

    Practiced importing images using assets

--- React/components/src/App.js
+++ React/components/src/App.js
@@ -1,28 +1,43 @@
-// * Practice with using React Router
-import "./App.css";
-import Homepage from "./Homepage";
-import AboutLittleLemon from "./AboutLittleLemon";
-import { Routes, Route, Link } from "react-router-dom";
-import Contact from "./Contact";
+// Practice using Assets in React
+import logo from "./assets/logo.png"
 
 function App() {
   return (
-    <div> 
-	  <nav>
-      <Link to="/" className="nav-item">Homepage</Link>
-      <Link to="/about" className="nav-item">About Little Lemon</Link>
-      <Link to="/contact" className="nav-item">Contact</Link>
-	  </nav>
-      <Routes> 
-        <Route path="/" element={<Homepage />}></Route>
-        <Route path="/about" element={<AboutLittleLemon />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
+    <div className="App">
+      <h1>Task: Add an image below</h1>
+      <img src={logo} alt="Logo" />
     </div>
   );
 };
 
 export default App;
+
+// * Practice with using routes
+
+// import "./App.css";
+// import Homepage from "./Homepage";
+// import AboutLittleLemon from "./Completed/AboutLittleLemon";
+// import { Routes, Route, Link } from "react-router-dom";
+// import Contact from "./Contact";
+
+// function App() {
+//   return (
+//     <div> 
+// 	  <nav>
+//       <Link to="/" className="nav-item">Homepage</Link>
+//       <Link to="/about" className="nav-item">About Little Lemon</Link>
+//       <Link to="/contact" className="nav-item">Contact</Link>
+// 	  </nav>
+//       <Routes> 
+//         <Route path="/" element={<Homepage />}></Route>
+//         <Route path="/about" element={<AboutLittleLemon />}></Route>
+//         <Route path="/contact" element={<Contact />}></Route>
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;
 
 // *Practice using state
 
--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {BrowserRouter} from "react-router-dom"
+// import React from "react";
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import {BrowserRouter} from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <BrowserRouter>
+//             <App />
+//         </BrowserRouter>
+//     </React.StrictMode>
+// );
 
 // import React from 'react';
 // import ReactDOM from 'react-dom/client';
 // import App from './App';
 
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render( 
-//     <App />  
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( 
+    <App />  
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
