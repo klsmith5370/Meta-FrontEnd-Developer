e7a55a08ce1b0241233db4cb7370df18abcf0dde
Kourtney Smith <klsmith5370@gmail.com>
2024-08-22 16:34:31 -0400

    Created basic list in sorted order by calories

--- React/Advanced-React/Lists/src/DessertsList.js
+++ React/Advanced-React/Lists/src/DessertsList.js
@@ -1,6 +1,23 @@
 function DessertsList(props) {
-  // Implement the component here.
-  return null;
+  const lowCaloriesDesserts = props.data
+  .filter((dessert) => {
+    return dessert.calories < 500;
+  })
+  .sort((a, b) => {
+    return a.calories - b.calories;
+  })
+  .map((dessert) => {
+    return (
+      <li>{dessert.name} - {dessert.calories} cal</li>
+    )
+  })
+  return (
+    <div>
+      <ul>
+        {lowCaloriesDesserts}
+      </ul>
+    </div>
+  );
 }
 
 export default DessertsList;
