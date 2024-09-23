102f2dbe50ec0286e146a6170ebf9d70bbaf738b
Kourtney Smith <klsmith5370@gmail.com>
2024-09-23 19:01:28 -0400

    Practiced connecting handler to onChange prop to RadioGroup

--- React/Advanced-React/Radio-Component/src/Radio/index.js
+++ React/Advanced-React/Radio-Component/src/Radio/index.js
@@ -19,9 +19,18 @@ export const RadioGroup = ({ onChange, selected, children }) => {
 export const RadioOption = ({ value, checked, onChange, children }) => {
   // Hook up the onChange handler to call the onChange prop passed to RadioGroup
   // Also, make sure to pass the correct checked prop to the input element
+
   return (
     <div className="RadioOption">
-      <input id={value} type="radio" name={value} />
+      <input 
+        id={value} 
+        type="radio" 
+        name={value} 
+        checked={checked} 
+        onChange={(e) => {
+          onChange(e.target.value)
+        }} 
+      />
       <label htmlFor={value}>{children}</label>
     </div>
   );
